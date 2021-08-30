28b009651c4086f784a3ca570a414018712af99b
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-08-30 14:26:59 +0900

    events

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 new Vue({
   el: "#app",
-  data: {
-    person: {
-      name: "Hello World",
-      age: 19,
-    },
-    inputData: "hello",
-    type: "text",
-    link: "https://blog.naver.com/",
-  },
+  data: {},
   methods: {
-    getBlogLink(id) {
-      return this.link + id;
-    },
-    nextYear(greeting) {
-      return (
-        greeting +
-        "! " +
-        this.person.name +
-        "는 내년에 " +
-        (this.person.age + 1) +
-        "살 입니다."
-      );
-    },
-    otherMethod() {
-      this.nextYear();
+    submit() {
+      alert("subbmited");
+      console.log("subbmited");
     },
   },
 });
