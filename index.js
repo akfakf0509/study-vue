f18f303453b8ce38ca1eda429a4e90e87897b240
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-08-30 10:27:08 +0900

    data and method

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 new Vue({
   el: "#app",
   data: {
-    name: "Hello World",
+    person: {
+      name: "Hello World",
+      age: 19,
+    },
+  },
+  methods: {
+    nextYear(greeting) {
+      return (
+        greeting +
+        "! " +
+        this.person.name +
+        "는 내년에 " +
+        (this.person.age + 1) +
+        "살 입니다."
+      );
+    },
+    otherMethod() {
+      this.nextYear();
+    },
   },
 });
