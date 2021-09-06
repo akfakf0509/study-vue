15bf123d5437db98cac8fc935fb3bb6d16b72e04
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-09-06 09:56:19 +0900

    vue component

--- script.js
+++ script.js
@@ -1,19 +1,77 @@
+// Vue.component("hello-world", {
+//   template: `
+//     <div>
+//       Hello World
+//     </div>
+//   `,
+// });
+
+// Vue.component("hello-button", {
+//   template: `
+//     <div>
+//       {{name}}
+//       <hello-world></hello-world>
+//       <button @click="changeText">Click</button>
+//     </div>
+//     `,
+//   data() {
+//     return { name: "Hello" };
+//   },
+//   methods: {
+//     changeText() {
+//       this.name = "Hello World";
+//     },
+//   },
+// });
+
+const helloWorld = {
+  template: `
+    <div>
+      Hello World
+    </div>
+  `,
+};
+
+const helloButton = {
+  components: {
+    "hello-world": helloWorld,
+  },
+  template: `
+    <div>
+      {{name}}
+      <hello-world></hello-world>
+      <button @click="changeText">Click</button>
+    </div>
+    `,
+  data() {
+    return { name: "Hello" };
+  },
+  methods: {
+    changeText() {
+      this.name = "Hello World";
+    },
+  },
+};
+
 const app = new Vue({
   el: "#app",
-  data: { name: "Hello" },
-  methods: {
-    changeText() {
-      app1.name = "Hello World";
-    },
+  components: {
+    "hello-button": helloButton,
   },
+  // data: { name: "Hello" },
+  // methods: {
+  //   changeText() {
+  //     this.name = "Hello World";
+  //   },
+  // },
 });
 
-const app1 = new Vue({
-  el: "#app-1",
-  data: { name: "Hello1" },
-  methods: {
-    changeText() {
-      this.name = "Hello World1";
-    },
-  },
-});
+// const app1 = new Vue({
+//   el: "#app-1",
+//   // data: { name: "Hello" },
+//   // methods: {
+//   //   changeText() {
+//   //     this.name = "Hello World";
+//   //   },
+//   // },
+// });
