7277f2edd4f3c816def25bd8a23bd37fe656b75b
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-09-08 13:42:53 +0900

    Todo App 1

---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="checkbox" :checked="todo.checked" />
+    <span class="ms-1">{{ todo.text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
